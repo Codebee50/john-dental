e2a3777b991dcfc9557508d84827276eea6ee3cc
codebee50 <codebee345@outlook.com>
2026-02-28 22:09:12 -0500

    update meta title

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,6 +1,6 @@
 export const appConfig = {
     APP_NAME: "GleamArt",
     APP_LONG_NAME: "GleamArt",
-    APP_DESCRIPTION: "Gleam art, best dentist in all of Newyork"
+    APP_DESCRIPTION: "Gleam art, best dentist in all of New york"
 
 }
